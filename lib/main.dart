02aa3992652cc70db6a5002068e809c48b010bdd
unknown <huynhtanphat130201@gmail.com>
2023-04-24 10:06:26 +0700

    first-commit

---
+++ lib/main.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Sleeping app',
+      home: Scaffold(
+        body: LayoutBuilder(
+          builder: (context, constraints) => Container(
+            width: constraints.maxWidth,
+            height: constraints.maxHeight,
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/images/background-1.png'),
+                fit: BoxFit.cover,
+              ),
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 200.0),
+                  child: Image.asset(
+                    'assets/images/logo-1.png',
+                    width: 250,
+                    height: 250,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                      vertical: 16.0), // specify the vertical padding here
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment
+                        .spaceEvenly, // add this to evenly space the buttons
+                    children: [
+                      ElevatedButton(
+                        onPressed: () {},
+                        style: ElevatedButton.styleFrom(
+                          primary: Color.fromRGBO(117, 166, 193, 1),
+                          minimumSize: const Size(250, 40),
+                        ),
+                        child: const Text('GET STARTED'),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 8),
+                        child: ElevatedButton(
+                          onPressed: () {},
+                          style: ElevatedButton.styleFrom(
+                            primary: Color.fromRGBO(53, 70, 112, 1),
+                            minimumSize: const Size(250, 40),
+                          ),
+                          child: const Text('ALREADY HAVE AN ACCOUNT'),
+                        ),
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
